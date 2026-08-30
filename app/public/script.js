6987e87dc0903ea9de0730a8e26180a075e4aeb3
Sokko <sokkotkaptam7@gmail.com>
2026-08-30 16:40:58 +0200

    Refactor HTML structure and add canvas drawing functionality

---
+++ app/public/script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("canvas");
+
+function draw() {
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgb(200 0 0)";
+  ctx.fillRect(10, 10, 50, 50);
+
+  ctx.fillStyle = "rgb(0 0 200 / 50%)";
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+draw();

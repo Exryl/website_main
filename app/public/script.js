75030fd5c814b6ce092b8cf3dbb437faf3dfb078
Sokko <sokkotkaptam7@gmail.com>
2026-08-30 16:57:58 +0200

    Fix formatting in HTML, update canvas drawing logic, and adjust CSS indentation

--- app/public/script.js
+++ app/public/script.js
@@ -1,13 +1,13 @@
 const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 function draw() {
-  const ctx = canvas.getContext("2d");
-
-  ctx.fillStyle = "rgb(200 0 0)";
-  ctx.fillRect(10, 10, 50, 50);
-
-  ctx.fillStyle = "rgb(0 0 200 / 50%)";
-  ctx.fillRect(30, 30, 50, 50);
+  ctx.fillStyle = "rgb(255 255 255)";
+  ctx.beginPath();
+  ctx.moveTo(75, 50);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.fill();
 }
 
 draw();
